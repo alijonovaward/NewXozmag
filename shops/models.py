42be4724c5b07345f004f3e7a4392396f7bf4e02
Azamjon Alijonov <azamjon@Azamjons-MacBook-Air.local>
2026-02-16 16:29:11 +0500

    shop ga str methodi qo'shildi

--- shops/models.py
+++ shops/models.py
@@ -12,3 +12,6 @@ class Shop(models.Model):
 
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"{self.name} {self.location} {self.owner} {self.created_at} {self.updated_at}"
