bbc613024780273b4c7d3083acb8bc9069270583
Azamjon Alijonov <azamjon@Azamjons-MacBook-Air.local>
2026-02-16 15:49:46 +0500

    Shop modeli ham qo'shildi

--- shops/admin.py
+++ shops/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Shop
 
 # Register your models here.
+@admin.register(Shop)
+class ShopAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'location','owner', 'created_at', 'updated_at')
+    search_fields = ('name', 'location')
+    list_filter = ('owner',)
+    ordering = ('-created_at',)
--- shops/models.py
+++ shops/models.py
@@ -1,3 +1,16 @@
+from django.contrib.auth.models import User
 from django.db import models
 
 # Create your models here.
+class Shop(models.Model):
+    name = models.CharField(max_length=100)
+    location = models.CharField(max_length=100)
+
+    owner = models.ForeignKey(User, related_name='shops',
+                              on_delete=models.CASCADE,
+                              limit_choices_to={'profile__user_type': 'owner'})
+    employee = models.ManyToManyField(User, related_name='employee_shops',blank=True,
+                                   limit_choices_to={'profile__user_type': 'employee'})
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
