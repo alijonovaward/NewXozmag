10ad6b73b9a6bb0acd388e4b294f7576fcc8f784
Azamjon Alijonov <azamjon@Azamjons-MacBook-Air.local>
2026-02-16 16:26:13 +0500

    Profile va Shop modellari yaxshi bog'lanishga almashtirildi

--- accounts/models.py
+++ accounts/models.py
@@ -1,5 +1,6 @@
 from django.contrib.auth.models import User
 from django.db import models
+from shops.models import Shop
 
 # Create your models here.
 class Profile(models.Model):
@@ -15,6 +16,14 @@ class Profile(models.Model):
     user_type = models.CharField(max_length=100, choices=USER_TYPES)
 
     is_deleted = models.BooleanField(default=False)
+
+    shop = models.ForeignKey(
+        'shops.Shop',
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=True,
+        related_name='employees'
+    )
 
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
--- shops/models.py
+++ shops/models.py
@@ -9,8 +9,6 @@ class Shop(models.Model):
     owner = models.ForeignKey(User, related_name='shops',
                               on_delete=models.CASCADE,
                               limit_choices_to={'profile__user_type': 'owner'})
-    employee = models.ManyToManyField(User, related_name='employee_shops',blank=True,
-                                   limit_choices_to={'profile__user_type': 'employee'})
 
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
