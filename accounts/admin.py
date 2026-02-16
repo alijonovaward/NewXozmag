8d686bcf3b27c02dd232960810e821761d507b6d
Azamjon Alijonov <azamjon@Azamjons-MacBook-Air.local>
2026-02-16 15:29:54 +0500

    Profile modeli yozildi

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Profile
 
 # Register your models here.
+@admin.register(Profile)
+class ProfileAdmin(admin.ModelAdmin):
+    list_display = ('user', 'phone_number', 'location', 'user_type', 'created_at', 'updated_at')
+    list_filter = ('user_type', 'created_at', 'updated_at')
+    search_fields = ('user', 'phone_number', 'location')
+    ordering = ('-created_at',)
+    readonly_fields = ('created_at', 'updated_at')
--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,18 @@
+from django.contrib.auth.models import User
 from django.db import models
 
 # Create your models here.
+class Profile(models.Model):
+    USER_TYPES = (
+        ('superadmin', 'Superadmin'),
+        ('owner', 'Owner'),
+        ('employee', 'Employee'),
+    )
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+
+    phone_number = models.CharField(max_length=11)
+    location = models.CharField(max_length=100)
+    user_type = models.CharField(max_length=100, choices=USER_TYPES)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
